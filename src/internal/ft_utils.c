7cb599b0ec5648def2788c6dae01f2dd1b5eead5
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2024-01-18 04:05:31 -0300

    Fix: add submodules and the first validation of the input when you run tha game.  Changes to be committed: 	new file:   lib/42_libft 	new file:   lib/MLX42 	modified:   src/internal/ft_utils.c 	modified:   src/main.c

--- src/internal/ft_utils.c
+++ src/internal/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 03:45:09 by dande-je          #+#    #+#             */
-/*   Updated: 2024/01/18 03:45:12 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/01/18 04:02:21 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	ft_output_error(char *str_title, char *str_message)
 {
-	ft_help();
 	ft_putstr_fd("\033[0;31m", STDERR_FILENO);
 	ft_putstr_fd(str_title, STDERR_FILENO);
 	ft_putstr_fd("\033[0m - ", STDERR_FILENO);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 07:55:53 by dande-je          #+#    #+#             */
-/*   Updated: 2024/01/18 03:44:09 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/01/18 04:04:31 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 
 int32_t	main(int32_t argc, char **argv)
 {
-	if (argc < 2 || argc > 3)
+	(void)argv;
+	if (argc != 2)
 		ft_output_error("Invalid So long", \
-			"./so_long [path of the maps]");
+			"./so_long [path of the map]");
 	exit(EXIT_SUCCESS);
 }
